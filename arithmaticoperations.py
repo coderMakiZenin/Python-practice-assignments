ce4ac2b56c5f8a28f13f27ea0df28df0ae836265
Sejal RD <sejalrd.work@gmail.com>
2026-06-15 23:15:29 +0530

    Add files via upload
    This code shows arithmatic operations like addition, substarction, multiplication and division.

---
+++ arithmaticoperations.py
@@ -0,0 +1,40 @@
+print("first number")
+no1 = int(input())
+
+print("second number")
+no2 = int(input())
+
+Ans =no1 + no2
+
+print("Addition is:",Ans)      #addition
+
+print("first number")
+no1 = int(input())
+
+print("second number")
+no2 = int(input())
+
+Ans =no1 - no2
+
+print("Substraction is:",Ans)  #substraction
+
+print("first number")
+no1 = int(input())
+
+print("second number")
+no2 = int(input())
+
+Ans =no1 * no2
+
+print("Multiplication is:",Ans)  #multiplication
+
+print("first number")
+no1 = int(input())
+
+print("second number")
+no2 = int(input())
+
+Ans =no1/no2
+
+print("Division is:",Ans)        #Division
+
