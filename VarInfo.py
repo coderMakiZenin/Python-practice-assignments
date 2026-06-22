f1f8e415eca6aea16bf8fb2b16d431b48573bbec
Sejal RD <sejalrd.work@gmail.com>
2026-06-22 17:02:42 +0530

    Add files via upload
    In this code, we have shown different variables x, y and z. The code helps us understand the data type of the variables, fetch the memory address of variables and lastly the variable size in bytes

---
+++ VarInfo.py
@@ -0,0 +1,17 @@
+import sys
+
+x=10                          #int
+y=26.11                       #float
+z="Coder Maki Zenin"          #string (str)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(id(x))
+print(id(y))
+print(id(z))
+
+print(sys.getsizeof(x))
+print(sys.getsizeof(y))
+print(sys.getsizeof(z))
