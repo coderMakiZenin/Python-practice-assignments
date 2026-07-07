14a6e2fe45d221027cb73fe2332a5d76bc14d328
Sejal RD <sejalrd.work@gmail.com>
2026-07-08 01:25:46 +0530

    Add files via upload
    Uses Lambda and user defined reduce to check maximum and minimum from the list.

---
+++ LambdaReduce3.py
@@ -0,0 +1,20 @@
+ChkSmallest= lambda No1,No2:No1 if No1<No2 else No2
+
+def ReduceX(Task, Elements):
+    result=Elements[0]
+    for no in Elements:
+        result=Task(result,no)
+    return result
+
+def main():
+    No=int(input(" Enter the number of elements: "))
+    Data = []
+    for i in range(1,(No+1)):
+        Val=int(input("Enter the number: "))
+        Data.append(Val)
+
+    Result= ReduceX(ChkSmallest,Data)
+    print("The Smallest is: ", Result)
+
+if __name__=="__main__":
+    main()
